0fd79dbbfaa539c7303d830b7acbb171f78bdb71
thefogiscoming-napiso <sebastianleonemedel2016@gmail.com>
2026-07-09 20:47:24 -0300

    Update sandboxelscloner.js

--- sandboxelscloner.js
+++ sandboxelscloner.js
@@ -1,11 +1,28 @@
-runAfterLoad(function () {
-    if (!elements.cloner) return;
-
-    const oldTick = elements.cloner.tick;
-
-    elements.cloner.tick = function(pixel) {
+runAfterLoad(function() {
+elements.cloner.tick = function(pixel) {
+    if (settings.survivalClone) {
         for (let i = 0; i < 100; i++) {
-            oldTick(pixel);
+            var x = pixel.x + (Math.random() < 0.5 ? 1 : -1);
+            var y = pixel.y + (Math.random() < 0.5 ? 1 : -1);
+            if (isEmpty(x,y)) {
+                createPixel(settings.survivalClone,x,y);
+            }
         }
-    };
+    }
+    else {
+        for (var i = 0; i < adjacentCoords.length; i++) {
+            var coords = adjacentCoords[i];
+            var x = pixel.x + coords[0];
+            var y = pixel.y + coords[1];
+            if (!isEmpty(x,y,true)) {
+                if (pixelMap[x][y].clone) { pixel.clone = pixelMap[x][y].clone; break }
+                var element = pixelMap[x][y].element;
+                if (element === pixel.element || elements[pixel.element].ignore.indexOf(element) !== -1) { continue }
+                settings.survivalClone = element;
+                survivalSave();
+                break;
+            }
+        }
+    }
+};
 });
